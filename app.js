74bbb56ad6b3e434ec9850deedbd6cbf1f89d9c1
Charlie Haag <charlottehaag22@gmail.com>
2019-06-19 21:48:13 -0700

    change homepage path

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+var path = require('path');
 var cors = require('cors');
 const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
@@ -8,5 +9,9 @@ app.use(cors());
 
 appConfig.init(app);
 routeConfig.init(app);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 module.exports = app;
