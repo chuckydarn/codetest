57c033efa39611c70eb5c9f480b3decd025df192
Charlie Haag <charlottehaag22@gmail.com>
2019-06-19 21:55:48 -0700

    try to fix build errors

--- app.js
+++ app.js
@@ -5,13 +5,32 @@ var cors = require('cors');
 const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 
+// view engine setup
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 appConfig.init(app);
 routeConfig.init(app);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
